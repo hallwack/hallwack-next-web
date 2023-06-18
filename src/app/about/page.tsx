9b59f814c2e5d8b3056dbfb2106818ca310a1141
Raihan Adam <hallwack.id@gmail.com>
2023-06-18 20:45:26 +0700

    feat: create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+type Props = {};
+
+export const metadata: Metadata = {
+  title: "About",
+};
+
+export default function Page({}: Props) {
+  return <div>Page</div>;
+}
