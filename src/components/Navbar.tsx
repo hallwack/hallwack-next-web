6a15d69a81288a51b54e0274e47036a3ee1b6130
Raihan Adam <hallwack.id@gmail.com>
2023-06-19 00:37:33 +0700

    fix: wrap logo with link in navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import NavLink from "./NavLink";
 
 export default function Navbar() {
   return (
     <nav className="w-full bg-transparent px-16 h-20 flex justify-between items-center sticky border-b-[0.5px] border-zinc-400">
       <div>
-        <h1 className="text-slate-200 font-bold text-2xl tracking-wide">
-          Raihan Adam
+        <h1 className="font-bold text-3xl">
+          <Link href="/">Raihan Adam</Link>
         </h1>
       </div>
       <ul className="flex items-center gap-2 text-sm">
