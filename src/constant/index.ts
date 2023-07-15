f6b9db5b96c1683bc0ec247f6843194535b98342
Raihan Adam <hallwack.id@gmail.com>
2023-07-15 20:24:52 +0700

    feat: add constant data for skills page

---
+++ src/constant/index.ts
@@ -0,0 +1,74 @@
+interface DataType {
+  name: string;
+  img: string;
+  desc: string;
+}
+
+export const dataTools: DataType[] = [
+  {
+    name: "Neovim",
+    img: "/logos/neovim.svg",
+    desc: "Text Editor",
+  },
+  {
+    name: "Brave Browser",
+    img: "/logos/brave.svg",
+    desc: "Browser",
+  },
+  {
+    name: "Visual Studio Code",
+    img: "/logos/vscode.svg",
+    desc: "Text Editor",
+  },
+  {
+    name: "Git",
+    img: "/logos/git.svg",
+    desc: "Version Controlling System",
+  },
+];
+
+export const dataSkillsLang: DataType[] = [
+  {
+    name: "Javascript",
+    img: "/logos/javascript.svg",
+    desc: "Javascript",
+  },
+  {
+    name: "PHP",
+    img: "/logos/php.svg",
+    desc: "PHP",
+  },
+];
+
+export const dataSkillsLib: DataType[] = [
+  {
+    name: "React",
+    img: "/logos/react.svg",
+    desc: "React",
+  },
+  {
+    name: "Laravel",
+    img: "/logos/laravel.svg",
+    desc: "Laravel",
+  },
+  {
+    name: "NextJS",
+    img: "/logos/nextjs.svg",
+    desc: "Next Js",
+  },
+  {
+    name: "Express",
+    img: "/logos/express.svg",
+    desc: "Expressjs",
+  },
+  {
+    name: "TailwindCSS",
+    img: "/logos/tailwindcss.svg",
+    desc: "Tailwindcss",
+  },
+  {
+    name: "Bootstrap",
+    img: "/logos/bootstrap.svg",
+    desc: "Bootstrap",
+  },
+];
